4171aa5bc0365b901ab5a61066dcc77d6210a3e8
how2loveme <kimjinsub01@naver.com>
2024-01-07 01:09:46 +0900

    combinelatest체이닝

--- index.js
+++ index.js
@@ -18,33 +18,57 @@ const request = function(param) {
     })
 }
 
-const foo1 = Rx.Observable.combineLatest([
+const foo3 = Rx.Observable.combineLatest([
     request('1'),
     request('2'),
-    request('3')
-])
-
-const foo2 = foo1.map(([res1,res2,res3]) => {
-    return [res1,res2,res3].map((x) => x*3);
+    request('3'),
+]).subscribe(([res1,res2,res3]) => {
+    return Rx.Observable.combineLatest([
+        request(res1 * 2),
+        request(res2 * 2),
+        request(res3 * 2),
+    ])
+    .subscribe(([res4,res5,res6]) => {
+        console.log(`3res4: ${res4}`);
+        console.log(`3res5: ${res5}`);
+        console.log(`3res6: ${res6}`);
+    })
 })
 
-const subscription1 = foo1.subscribe(([res1,res2,res3]) => {
-    console.log(`1res1: ${res1}`);
-    console.log(`1res2: ${res2}`);
-    console.log(`1res3: ${res3}`);
-})
-const subscription2 = foo2.subscribe(([res1,res2,res3]) => {
-    console.log(`2res1: ${res1}`);
-    console.log(`2res2: ${res2}`);
-    console.log(`2res3: ${res3}`);
-})
-setTimeout(function() {
-    subscription1.unsubscribe();
-    console.log('1 구취')
-    subscription2.unsubscribe();
-    console.log('2 구취')
-
+console.log(foo3)
+setTimeout(() => {
+    foo3.unsubscribe();
+    console.log('3 구취')
+    console.log(foo3)
 }, 3000)
+
+// const foo1 = Rx.Observable.combineLatest([
+//     request('1'),
+//     request('2'),
+//     request('3')
+// ])
+//
+// const foo2 = foo1.map(([res1,res2,res3]) => {
+//     return [res1,res2,res3].map((x) => x*3);
+// })
+//
+// const subscription1 = foo1.subscribe(([res1,res2,res3]) => {
+//     console.log(`1res1: ${res1}`);
+//     console.log(`1res2: ${res2}`);
+//     console.log(`1res3: ${res3}`);
+// })
+// const subscription2 = foo2.subscribe(([res1,res2,res3]) => {
+//     console.log(`2res1: ${res1}`);
+//     console.log(`2res2: ${res2}`);
+//     console.log(`2res3: ${res3}`);
+// })
+// setTimeout(function() {
+//     subscription1.unsubscribe();
+//     console.log('1 구취')
+//     subscription2.unsubscribe();
+//     console.log('2 구취')
+//
+// }, 3000)
 
 // request('1').subscribe((res) => {
 //     console.log(`kowaine: ${res}`)
